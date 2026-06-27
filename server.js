b91739a2946ee4f1e871c646250ad71aeaebc152
ngwasidwan <sidwanche@gmail.com>
2026-06-27 03:57:35 +0100

    added fs module

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const http = require("http");
+const fs = require("fs");
+
+fs.readFile("./README.md", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
 const server = http.createServer((req, res) => {
   if (req.url === "/") res.end("hello");
+  if (req.url === "/login") res.end("login route");
 });
 
 server.listen(5000, () => console.log("server listening on port 5000"));
