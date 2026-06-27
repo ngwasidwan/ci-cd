2fabfbc652bd1c90b1e18508bf1fa5d3db82bea5
ngwasidwan <sidwanche@gmail.com>
2026-06-27 04:12:31 +0100

    added new routes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const http = require("http");
 const fs = require("fs");
+const crypto = require("crypto");
 
 fs.readFile("./README.md", (err, data) => {
   if (err) throw err;
   console.log(data.toString());
 });
 const server = http.createServer((req, res) => {
-  if (req.url === "/home") res.end("this is the home page");
+  if (req.url === "/login") res.end("this is the login page");
+  if (req.url === "/") res.end("this is the home page");
+  if (req.url === "/logout") res.end("this is the logout page");
 });
 
 server.listen(5000, () => console.log("server listening on port 5000"));
