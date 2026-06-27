d6a3eeb96c3394ed4e071172cc318cacfe8c343b
Ngwa Sidwan <131943139+ngwasidwan@users.noreply.github.com>
2026-06-27 03:46:15 +0100

    added /home route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url === "/") res.end("hello");
+  if(req.url === "/home") res.end('this is the home page')
 });
 
 server.listen(5000, () => console.log("server listening on port 5000"));
